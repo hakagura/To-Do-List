6e77a4abe8d5b0c55d0015c11a128862bee6a866
esmerino costa <hakagura@gmail.com>
2012-02-02 18:40:58 -0300

    Adding before create for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  before_create :set_role
+
+  def set_role
+    self.role="user"
+  end
 end
