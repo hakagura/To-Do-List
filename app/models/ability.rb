6b10fdb3d87fc11196e7866c2e9aba751edd03f7
esmerino costa <hakagura@gmail.com>
2012-02-03 18:47:16 -0300

    Cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,13 @@
-class Ability # Class com as definicoes das barreiras do cancan
+class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new
-    if user.role == 'admin' # se usuario administrador pode modificiar tudo
+    if user.role == 'admin'
       can :manage, :all
     elsif user.role == 'user'
-      can :manage, [Project, List, Task], :user_id => user.id # modifica somente se for o usuario de criacao
-      can [:index, :show, :edit, :update], User, :id => user.id # modifica somente se for o usuario de criacao
+      can :manage, [Project, List, Task], :user_id => user.id 
+      can [:index, :show, :edit, :update,:closed_task], User, :id => user.id
     else
       can :read, User
     end
