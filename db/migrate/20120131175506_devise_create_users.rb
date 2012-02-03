2938c885f3f824351302faafd111828bbc5e6693
esmerino costa <hakagura@gmail.com>
2012-02-02 21:49:52 -0300

    Adding confirmable

--- db/migrate/20120131175506_devise_create_users.rb
+++ db/migrate/20120131175506_devise_create_users.rb
@@ -23,9 +23,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string :password_salt
 
       ## Confirmable
-      # t.string   :confirmation_token
+      t.string   :confirmation_token
       # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
+      t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
@@ -42,7 +42,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
+    add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
   end
