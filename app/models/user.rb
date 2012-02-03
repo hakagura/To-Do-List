5925b7dac112d2d10dee422aca4c1ce4ec5329f5
esmerino costa <hakagura@gmail.com>
2012-02-02 23:40:25 -0300

    Devise options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,:recoverable, :confirmable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+        :token_authenticatable, :confirmable, :lockable, :timeoutable 
 
   #Associations
   has_many :projects
